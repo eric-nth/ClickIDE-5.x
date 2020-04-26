d8bef1fb82b81eb217e9e76bf48a22b208d7a4d8
EricNTH080103 <eric_ni2008@163.com>
2020-04-26 10:32:51 +0800

    5.0.0.28
    Publish. Bug fixed.

--- ClickIDE_private.h
+++ ClickIDE_private.h
@@ -5,19 +5,19 @@
 #define CLICKIDE_PRIVATE_H
 
 /* VERSION DEFINITIONS */
-#define VER_STRING	"5.0.0.27"
+#define VER_STRING	"5.0.0.28"
 #define VER_MAJOR	5
 #define VER_MINOR	0
 #define VER_RELEASE	0
-#define VER_BUILD	27
+#define VER_BUILD	28
 #define COMPANY_NAME	""
-#define FILE_VERSION	"5.0.0.27"
+#define FILE_VERSION	"5.0.0.28"
 #define FILE_DESCRIPTION	"ClickIDE 5.0.0 - Stable"
 #define INTERNAL_NAME	""
 #define LEGAL_COPYRIGHT	""
 #define LEGAL_TRADEMARKS	""
 #define ORIGINAL_FILENAME	""
 #define PRODUCT_NAME	""
-#define PRODUCT_VERSION	"5.0.0.27"
+#define PRODUCT_VERSION	"5.0.0.28"
 
 #endif /*CLICKIDE_PRIVATE_H*/
